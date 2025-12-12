68d970904e14116179e3f4e666fc8de3c93b16d0
Taeze1000 <Taezeted@gmail.com>
2025-12-12 14:07:03 -0700

    Add SounmdManager
    added a soundmanager and soundfile singlton

---
+++ Singletons/Audio/SoundFiles.gd
@@ -0,0 +1,16 @@
+extends Node
+
+#region SFX
+var button_down = "res://Assests/Audio/Button/button_down.mp3"
+var button_release = "res://Assests/Audio/Button/button_release.mp3"
+var coins = ["res://Assests/Audio/Coins/coin1.mp3", "res://Assests/Audio/Coins/coin2.mp3", "res://Assests/Audio/Coins/coin3.mp3", "res://Assests/Audio/Coins/coin4.mp3", "res://Assests/Audio/Coins/coin5.mp3"]
+var dragon_sleep = "res://Assests/Audio/Dragon/dragon_sleep.mp3"
+var dragon_fire = "res://Assests/Audio/Dragon/Fire sound.mp3"
+var dragon_tail_swip = "res://Assests/Audio/Dragon/tail_swip.mp3"
+#endregion
+
+#region music
+var backround_1 = "res://Assests/Audio/Music/Background Music Main.mp3"
+var backround_2 = "res://Assests/Audio/Music/Main Backgrouns Music Final.mp3"
+var boss_music = "res://Assests/Audio/Music/Boss Song.mp3"
+#endregion
