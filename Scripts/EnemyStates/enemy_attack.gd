0657a7770f9600cec18ff5c2fe33a25b3c3d4b19
Taeze1000 <Taezeted@gmail.com>
2025-12-13 11:45:01 -0700

    enemy flip H/enemy footstep sound

--- Scripts/EnemyStates/enemy_attack.gd
+++ Scripts/EnemyStates/enemy_attack.gd
@@ -1,8 +1,7 @@
 extends EnemyState
 
-@export var attack_cooldown:float = 0.5
-@export var attack_distance:int = 100
 @onready var timer:Timer = $Timer
+var facing:bool
 
 func enter(_previous_state_path: String, _data:Dictionary):
 	player = _data["player"]
@@ -10,14 +9,16 @@ func enter(_previous_state_path: String, _data:Dictionary):
 func phys_update(_delta: float):
 	if not timer.is_stopped():
 		return
-	if abs(player.global_position.x - get_parent().global_position.x) >= attack_distance:
+	var direction_to_player = player.global_position.x - get_parent().global_position.x
+	if abs(player.global_position.x - get_parent().global_position.x) >= get_parent().attack_distance:
 		finished.emit("EnemyChase", {"player": player})
-	get_parent().change_animation(animation_sprite)
-	timer.start(attack_cooldown)
-	print("attack start")
+	if direction_to_player > 0:
+		facing = true
+	elif direction_to_player <= 0:
+		facing = false
+	get_parent().change_animation(animation_sprite, facing)
+	timer.start(get_parent().attack_cooldown)
 	await timer.timeout
-	print("attack done")
-	if abs(player.global_position.x - get_parent().global_position.x) <= attack_distance:
+	if abs(player.global_position.x - get_parent().global_position.x) <= get_parent().attack_distance:
 		var player_health:HealthComponent = player.find_child("HealthComponent")
 		player_health.adjust_health(-get_parent().damage)
-	
--- Scripts/EnemyStates/enemy_chase.gd
+++ Scripts/EnemyStates/enemy_chase.gd
@@ -1,25 +1,36 @@
 extends EnemyState
 
 var direction_to_player
-
-@export var attack_distance := 100
-@export var loose_player_distance := 700
+var facing_left:bool
+@onready var audio:AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func enter(_previous_state_path: String, _data:Dictionary):
 	player = _data["player"]
 	direction_to_player = player.global_position.x - get_parent().global_position.x
 	if direction_to_player > 0:
 		direction_to_player = 1
+		facing_left = true
 	elif direction_to_player <= 0:
 		direction_to_player = -1
-	get_parent().change_animation(animation_sprite)
+		facing_left = false
+	get_parent().change_animation(animation_sprite, facing_left)
 	enemy_body = get_parent().get_parent()
+	audio.stream = load(SoundFiles.snowy_footsteps.pick_random())
+	audio.play()
+	audio.set_stream_paused(false)
 
 
 func phys_update(_delta: float):
 	enemy_body.position.x += get_parent().move_speed * _delta * direction_to_player
 	
-	if abs(player.global_position.x - get_parent().global_position.x) <= attack_distance:
+	if abs(player.global_position.x - get_parent().global_position.x) <= get_parent().attack_distance:
 		finished.emit("EnemyAttack", {"player": player})
-	elif abs(player.global_position.x - get_parent().global_position.x) >= loose_player_distance:
+	elif abs(player.global_position.x - get_parent().global_position.x) >= get_parent().loose_player_distance:
 		finished.emit("EnemyIdle")
+
+func exit() -> void:
+	audio.set_stream_paused(true)
+
+func _on_audio_stream_player_2d_finished() -> void:
+	audio.stream = load(SoundFiles.snowy_footsteps.pick_random())
+	audio.play()
