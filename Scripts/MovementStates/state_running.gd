3f1dfdcdc76dcd1828898d0b61784146949b274d
Kacz <zdavey@gmail.com>
2025-12-12 20:01:00 -0700

    a

--- Scripts/MovementStates/state_running.gd
+++ Scripts/MovementStates/state_running.gd
@@ -4,6 +4,7 @@ var speed
 func enter(_previous_state_path: String, _data := {}):
 	# cancel out any momentum
 	# play idle animation
+	parent.animated_sprite.play("running")
 	speed = parent.move_speed
 
 func phys_update(_delta: float):
@@ -11,6 +12,11 @@ func phys_update(_delta: float):
 	player_body.velocity.y += gravity * _delta
 	# Start running based on player input
 	var horizontal_input = Input.get_axis("Left", "Right")
+	# Flip sprite by direction
+	if horizontal_input < 0:
+		parent.animated_sprite.flip_h = true
+	elif horizontal_input > 0:
+		parent.animated_sprite.flip_h = false
 	player_body.velocity.x = horizontal_input * speed
 	# Stop running go idle
 	if is_equal_approx(horizontal_input, 0.0):
