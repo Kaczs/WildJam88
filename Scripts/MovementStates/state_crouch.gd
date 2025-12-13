2d6666724e6254fad54f3e51cee22d205eaa4959
Kacz <zdavey@gmail.com>
2025-12-12 20:28:54 -0700

    crouching state

---
+++ Scripts/MovementStates/state_crouch.gd
@@ -0,0 +1,30 @@
+extends MovementState
+var speed
+
+func enter(_previous_state_path: String, _data := {}):
+	# cancel out any momentum
+	# play idle animation
+	parent.animated_sprite.play("crouching")
+	speed = parent.move_speed
+
+func phys_update(_delta: float):
+	# Gravity
+	player_body.velocity.y += gravity * _delta
+	# Start running based on player input
+	var horizontal_input = Input.get_axis("Left", "Right")
+	# Flip sprite by direction
+	if horizontal_input < 0:
+		parent.animated_sprite.flip_h = true
+	elif horizontal_input > 0:
+		parent.animated_sprite.flip_h = false
+	player_body.velocity.x = horizontal_input * speed
+	# Stopped crouching when trying to run? Stand and run
+	if not Input.is_action_pressed("Down") and abs(horizontal_input) > 0:
+		finished.emit("StateRunning")
+	# Stopped crouching while idle
+	elif not Input.is_action_pressed("Down"):
+		finished.emit("StateIdle")
+	# Trying to jump?
+	elif Input.is_action_pressed("Up") and player_body.is_on_floor():
+		finished.emit("StateJumping")
+	player_body.move_and_slide()
