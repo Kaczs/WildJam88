3aac58f85bba59215c376c9d148f765539291fdc
Tidysmile010 <swtwhitehead@gmail.com>
2025-12-12 19:39:13 -0800

    Footstep Audio

--- Singletons/Audio/SoundFiles.gd
+++ Singletons/Audio/SoundFiles.gd
@@ -1,16 +1,9 @@
 extends Node
 
 #region SFX
-var button_down = "res://Assests/Audio/Button/button_down.mp3"
-var button_release = "res://Assests/Audio/Button/button_release.mp3"
-var coins = ["res://Assests/Audio/Coins/coin1.mp3", "res://Assests/Audio/Coins/coin2.mp3", "res://Assests/Audio/Coins/coin3.mp3", "res://Assests/Audio/Coins/coin4.mp3", "res://Assests/Audio/Coins/coin5.mp3"]
-var dragon_sleep = "res://Assests/Audio/Dragon/dragon_sleep.mp3"
-var dragon_fire = "res://Assests/Audio/Dragon/Fire sound.mp3"
-var dragon_tail_swip = "res://Assests/Audio/Dragon/tail_swip.mp3"
+var snowy_footsteps_player ="res://Assests/SFX/fast_footsteps_on_snow_Final.mp3"
 #endregion
 
 #region music
-var backround_1 = "res://Assests/Audio/Music/Background Music Main.mp3"
-var backround_2 = "res://Assests/Audio/Music/Main Backgrouns Music Final.mp3"
-var boss_music = "res://Assests/Audio/Music/Boss Song.mp3"
+
 #endregion
