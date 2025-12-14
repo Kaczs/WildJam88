183806733a9dda06f615b3ff8cf1665ae53979eb
Taeze1000 <Taezeted@gmail.com>
2025-12-13 19:44:10 -0700

    updated health comp to work with state machines

--- Scripts/health_component.gd
+++ Scripts/health_component.gd
@@ -2,17 +2,32 @@ class_name HealthComponent extends Node
 
 @export var max_health := 100
 var current_health
+var is_dead := false
+
+signal on_hit(stagger_duration:float, is_dead:bool)
 
 func _ready():
 	current_health = max_health
 
-## Adjust's health by the given value, provide negative value to deal damage, positive for healing
-func adjust_health(amount):
+##Heal by the given value
+func heal(amount):
 	current_health += amount
+	if current_health > max_health:
+		current_health = max_health
+
+##Deal damage by given amount
+func take_damage(amount:int, stagger:float = 0):
+	current_health -= amount
 	if current_health <= 0:
-		print("I ded :" + get_parent().name)
-		# if player change to die
-		# if bot change to die state
-		# if none of the above delete it (like static boxes for instance?)
+		is_dead = true
+	on_hit.emit(stagger, is_dead)
+
+##Change max_health by given amount
+func adjust_max_health(amount):
+	max_health += amount
+	if max_health <= 0:
+		max_health = 1
+		current_health = 1
+		return
 	if current_health > max_health:
 		current_health = max_health
