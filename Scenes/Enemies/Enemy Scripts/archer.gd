036a3b2bf9200f942ef692fe54adffe6955b2ada
Taeze1000 <Taezeted@gmail.com>
2025-12-14 18:40:26 -0700

    archer shoots

--- Scenes/Enemies/Enemy Scripts/archer.gd
+++ Scenes/Enemies/Enemy Scripts/archer.gd
@@ -2,8 +2,13 @@ extends RigidBody2D
 
 var projectile:PackedScene = load("res://Scenes/Enemy Components/projectile.tscn")
 
-@export var force_of_shot := Vector2(100, 100)
+@export var force_of_shot := 700
+@export var direction:int
 
-func shoot():
+
+func hit_player():
 	var arrow:Projectile = projectile.instantiate()
-	
+	arrow.force = force_of_shot * direction
+	arrow.global_position = self.global_position
+	arrow.damage = get_node("EnemyBrainComponent").damage
+	get_parent().add_child(arrow)
--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -1,17 +1,19 @@
-class_name Projectile extends RigidBody2D
+class_name Projectile extends Node2D
 
-@export var force := Vector2(0,700)
+@export var force := 500
 @export var damage:int
 @export var despawn_timer := 3.3
 
 func _ready() -> void:
-	apply_central_force(force)
 	$DespawnTimer.start(despawn_timer)
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		body.get_node("HealthComponent").deal_damage(damage)
+		body.get_node("HealthComponent").take_damage(damage)
 		$AnimationPlayer.play("Hit")
 
 func die():
 	self.queue_free()
+
+func _physics_process(delta: float) -> void:
+	position.x += force * delta
