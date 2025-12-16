a406009ade6040ab79e916386e71c71464e9c633
Kacz <zdavey@gmail.com>
2025-12-16 16:27:12 -0700

    prelimary states

---
+++ Scripts/MovementStates/state_parry.gd
@@ -0,0 +1,35 @@
+extends MovementState
+var parry_area:Area2D
+
+func enter(_previous_state_path: String, _data := {}):
+	# Flip hitbox depending on facing
+	if sprite.flip_h == true:
+		parent.flip_character()
+	animation_player.play("parry")
+
+func phys_update(_delta: float):
+	# Gravity
+	player_body.velocity.y += gravity * _delta
+	# Start running based on player input
+	var horizontal_input = Input.get_axis("Left", "Right")
+# Stuff we need to cancel into
+	if parent.cannot_cancel == false:
+		if abs(horizontal_input) > 0:
+			finished.emit("StateRunning")
+		# Next attack in the chain
+		elif Input.is_action_just_pressed("Attack"):
+			finished.emit("StateStandingAttack2")
+		# Trying to jump?
+		elif Input.is_action_pressed("Up") and player_body.is_on_floor():
+			finished.emit("StateJumping")
+		# Optionally immediately animation: X X X 
+		# if hit
+		# 0.16s after animation then unlock player > nother attack > jump > move > parry
+		# kill 0.16s delay if hit a guy
+# Can freely cancel anytime
+	elif Input.is_action_just_pressed("special1"):
+		finished.emit("StateRadiantDash")
+	player_body.move_and_slide()
+
+func exit() -> void:
+	player_body.velocity.x = 0
