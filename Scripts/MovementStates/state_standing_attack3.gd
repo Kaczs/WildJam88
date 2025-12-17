815565c05fa0f651882641f368df233b0a21bef8
Kacz <zdavey@gmail.com>
2025-12-17 02:13:14 -0700

    three hit combo

--- Scripts/MovementStates/state_standing_attack3.gd
+++ Scripts/MovementStates/state_standing_attack3.gd
@@ -4,7 +4,7 @@ func enter(_previous_state_path: String, _data := {}):
 	# Flip hitbox depending on facing
 	if sprite.flip_h == true:
 		parent.flip_character()
-	animation_player.play("standingattack2")
+	animation_player.play("standingattack3")
 
 func phys_update(_delta: float):
 	# Gravity
@@ -12,19 +12,16 @@ func phys_update(_delta: float):
 	# Slide the player forward as they attack
 	if parent.slide_forward == true:
 		if sprite.flip_h == true:
-			player_body.velocity.x = -300.0
+			player_body.velocity.x = -600.0
 		else:
-			player_body.velocity.x = 300.0
+			player_body.velocity.x = 600.0
 	else:
 		player_body.velocity.x = 0
 # Stuff we need to Cancel into
 	if parent.cannot_cancel == false:	
 		var horizontal_input = Input.get_axis("Left", "Right")
-		# Next attack in sequence
-		if Input.is_action_pressed("Attack"):
-			finished.emit("StateStandingAttack3")
 		# Start running based on player input
-		elif abs(horizontal_input) > 0:
+		if abs(horizontal_input) > 0:
 			finished.emit("StateRunning")
 		# Trying to jump?
 		elif Input.is_action_pressed("Up") and player_body.is_on_floor():
