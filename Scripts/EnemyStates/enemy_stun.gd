92f021187ce77db6f87dd297ac7e9932ff7cddd8
Taeze1000 <Taezeted@gmail.com>
2025-12-19 10:28:17 -0700

    Parrying enemy now turns them blue

--- Scripts/EnemyStates/enemy_stun.gd
+++ Scripts/EnemyStates/enemy_stun.gd
@@ -1,22 +1,23 @@
 extends EnemyState
 
+@export var animation_sprite2:String
 @onready var timer:Timer =$Timer
-var stunned:=false
+var parried:=false
 
 
 func enter(_previous_state_path: String, _data:Dictionary):
-	stunned = true
 	timer.start(_data["stun duration"])
-	animation_player.play(animation_sprite)
+	if _data.has("parried"):
+		animation_player.play(animation_sprite2)
+	else:
+		animation_player.play(animation_sprite)
+
 
 func _on_timer_timeout() -> void:
-	if not stunned:
-		push_warning(enemy_body.name + " Tried to leveave stun while not stunned")
-		return
 	finished.emit("EnemyReturn")
 	print("HEY THE TIMER TIMED OUT")
 
 func exit() -> void:
-	stunned = false
+	parried = false
 	#ensuers that if the enemy dies befor timer timout that _on_timer_timeout is not called
 	timer.stop()
