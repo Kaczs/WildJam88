6dd0f5fc3018a57dcfd57fcca592a9567d46446d
Taeze1000 <Taezeted@gmail.com>
2025-12-18 20:29:37 -0700

    added a check to see if enemy is leaving stun when out stunned

--- Scripts/EnemyStates/enemy_stun.gd
+++ Scripts/EnemyStates/enemy_stun.gd
@@ -1,15 +1,22 @@
 extends EnemyState
 
 @onready var timer:Timer =$Timer
+var stunned:=false
+
 
 func enter(_previous_state_path: String, _data:Dictionary):
+	stunned = true
 	timer.start(_data["stun duration"])
 	animation_player.play(animation_sprite)
 
 func _on_timer_timeout() -> void:
+	if not stunned:
+		push_warning(enemy_body.name + " Tried to leveave stun while not stunned")
+		return
 	finished.emit("EnemyReturn")
 	print("HEY THE TIMER TIMED OUT")
 
 func exit() -> void:
+	stunned = false
 	#ensuers that if the enemy dies befor timer timout that _on_timer_timeout is not called
 	timer.stop()
