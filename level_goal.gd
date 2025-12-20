d56199b75162cd82a0c3f362b9173c266a4083f6
Kacz <zdavey@gmail.com>
2025-12-19 18:56:13 -0700

    tutorial finalization

--- level_goal.gd
+++ level_goal.gd
@@ -1,7 +1,8 @@
 extends Area2D
-
+@export var next_scene:String
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("Player Wins Level")
+		get_tree().change_scene_to_file(next_scene)
 		#Progress to next
