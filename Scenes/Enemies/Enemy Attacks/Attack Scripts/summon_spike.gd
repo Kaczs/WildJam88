18d4d45d576e15de0bc597c26c0769f887a27f70
Taeze1000 <Taezeted@gmail.com>
2025-12-20 13:26:04 -0700

    ice spike spawns on player

--- Scenes/Enemies/Enemy Attacks/Attack Scripts/summon_spike.gd
+++ Scenes/Enemies/Enemy Attacks/Attack Scripts/summon_spike.gd
@@ -9,10 +9,10 @@ var spike_scene:PackedScene = load("res://Scenes/Enemies/Enemy Attacks/Attack Sc
 
 func attack(player:CharacterBody2D, animation_player:AnimationPlayer, _enemy:CharacterBody2D):
 	var spike:Spike = spike_scene.instantiate()
-	spike.global_position = player.global_position
 	spike.scale = Vector2(0.2,0.2)
 	spike.damage = damage
 	spike.show_behind_parent = true
 	animation_player.play("Summon")
 	await animation_player.summon
+	spike.global_position = player.global_position
 	player.get_parent().add_child(spike)
