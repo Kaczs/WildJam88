d4d3d6d47af374f88308213815000f343b90336b
Taeze1000 <Taezeted@gmail.com>
2025-12-20 18:41:14 -0700

    stopped working on slime

--- Scenes/Enemies/Enemy Scripts/animation_player.gd
+++ Scenes/Enemies/Enemy Scripts/animation_player.gd
@@ -2,5 +2,14 @@ extends AnimationPlayer
 
 signal summon
 
+@onready var enemy:CharacterBody2D = $".."
+var pos
+
 func summon_now():
 	summon.emit()
+
+func move():
+	pos = enemy.position
+	self.play("RESET")
+	self.advance(0)
+	enemy.position = pos
